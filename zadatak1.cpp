0ea4dbc4cbbf0d4a6d3cb891be28e63120c702d4
anitaprokic <aprokic04@gmail.com>
2026-07-06 12:08:14 +0200

    Fix include typo and update task comments
    Updated comments and fixed include typo in zadatak1.cpp.

--- zadatak1.cpp
+++ zadatak1.cpp
@@ -1,9 +1,10 @@
-#inlcude <iostream>
+#include <iostream>
 #include <stack>
 using namespace std;
 int main(){
-//Zadatak vredi 6 boda
-//Napisati program koji kreira stek celih brojeva, ubaciti brojeve 23, 28, 33, 42.
-//Prikazati element sa vrha
+//Zadatak vredi 10 boda
+//Napisati program koji kreira stek celih brojeva, ubaciti brojeve 23, 28, 33, 42 - 5 boda
+//Prikazati element sa vrha - 2 boda
+//Ukloniti dva elemeta sa vrha i prikazati trenutni element na vrhu - 3 boda
   return 0;
 }
