4c9c0660dc4d16727239adf93239405a28176de9
anitaprokic <aprokic04@gmail.com>
2026-07-06 12:02:18 +0200

    Correct typos in zadatak1.cpp
    Fix typos in include directives and namespace usage.

---
+++ zadatak1.cpp
@@ -0,0 +1,9 @@
+#inlcude <iosteam>
+#include <stack>
+usinig namespace std;
+int main(){
+//Zadatak vredi 6 boda
+//Napisati program koji kreira stek celih brojeva, ubaciti brojeve 23, 28, 33, 42.
+//Prikazati element sa vrha
+  return 0;
+}
